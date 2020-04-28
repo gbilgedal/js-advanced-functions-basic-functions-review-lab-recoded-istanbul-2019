f1fa6e7db2cff14d4c7f9593559ebfdc4a760049
G Bilge Dal <lagala999@gmail.com>
2020-04-28 10:38:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 // Your code here
-function mondayWork(work){
-  if(work)
-    console.log("This monday, I will "+work);
-  else
-    console.log("This monday, I will go to the office");
+function saturdayFun (activity = 'roller-skate'){
+return "This Saturday, I want to "+ activity;
+}
 
+let mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
 
+function wrapAdjective (wrapper = '*'){
+    return function (string = 'special') {
+        return `You are ${wrapper}${string}${wrapper}!`;
+    }
+}
 
+const Calculator = {};
+
+Calculator.add = function (a,b){
+return a + b;
+}
+
+Calculator.subtract = function (a,b){
+    return a - b;
+}
+
+Calculator.multiply = function (a,b){
+    return a * b;
+}
+
+Calculator.divide = function (a,b){
+    return a / b;
+}
+
+function actionApplyer (start, Arr){
+    for (let i = 0 ; i < Arr.length; i++){
+       start = Arr[i](start);
+    }
+    return start;
 }
