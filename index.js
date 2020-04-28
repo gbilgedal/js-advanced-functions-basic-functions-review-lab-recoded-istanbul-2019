aeaaccafcb27ce6373862683d18a9360f70692e5
G Bilge Dal <lagala999@gmail.com>
2020-04-28 10:39:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ return "This Saturday, I want to "+ activity;
 }
 
 let mondayWork = function (activity = 'go to the office'){
-    return "This Monday, I will "+ activity ;
+    return "This Monday, I will "+ activity+"." ;
 }
 
 function wrapAdjective (wrapper = '*'){
