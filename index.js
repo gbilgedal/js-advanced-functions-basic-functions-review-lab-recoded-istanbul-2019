e0fdc7b3c20e193da7b038df8aa197f4dbed35b6
G Bilge Dal <lagala999@gmail.com>
2020-04-28 10:39:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 function saturdayFun (activity = 'roller-skate'){
-return "This Saturday, I want to "+ activity;
+return "This Saturday, I want to "+ activity+"!";
 }
 
 let mondayWork = function (activity = 'go to the office'){
